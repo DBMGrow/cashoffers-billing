64a8bb7f6a7b9a3f478129668c0b98616c2bee32
davidcrammer <59569187+davidcrammer@users.noreply.github.com>
2025-10-07 11:49:26 -0700

    Refactor code structure for improved readability and maintainability

--- src/database/database.js
+++ src/database/database.js
@@ -1,5 +1,5 @@
 import { Sequelize } from "sequelize"
-import config from "../../config/config.js"
+import config from "../config/config.js"
 
 const env = process.env.NODE_ENV || "development"
 const db = config?.[env].database
